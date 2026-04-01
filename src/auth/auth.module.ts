fa121e4399205e6100845b54af968d2c84ef22c4
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2026-04-01 10:43:54 -0700

    feat: implement jwt tokens to login add jwt tokens to auth/login for safe access endpoint seems to work just fine

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+  ) {}
+
+  async login(email: string, pass: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException('Correo o contraseña incorrectos');
+    }
+    const isPasswordValid = await bcrypt.compare(pass, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Correo o contraseña incorrectos');
+    }
+
+    const payload = { 
+      sub: user.id, 
+      email: user.email, 
+      tenantId: user.tenantId 
+    };
+    
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        tenantId: user.tenantId
+      }
+    };
+  }
+}
