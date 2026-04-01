a08b866569f85c8cd4745cb44a4d885076713cfb
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2026-03-31 19:19:57 -0700

    feat: create controllers, service and dto for user
    add name to User model on schema.prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Este correo electrónico ya está en uso.');
+    }
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    return await this.prisma.user.create({
+      data: {
+        email: createUserDto.email,
+        password: hashedPassword,
+        name: createUserDto.name,
+        tenantId: createUserDto.tenantId,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        tenantId: true,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findAllByTenant(tenantId: string) {
+    return await this.prisma.user.findMany({
+      where: { tenantId },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
   }
 }
