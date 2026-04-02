86c3213ebd33fe135f0a7c8ebd60ced99550a1f7
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2026-04-01 17:58:28 -0700

    feat: create notifications module websockets for real time communications w/ arduino
    just add basic Nestjs structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { UsersModule } from './users/users.module';
 import { TenantsModule } from './tenants/tenants.module';
 import { AuthModule } from './auth/auth.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, DevicesModule, EventsModule, DashboardModule, UsersModule, TenantsModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, DevicesModule, EventsModule, DashboardModule, UsersModule, TenantsModule, AuthModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
